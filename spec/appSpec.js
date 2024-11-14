e18f3f7db5719815503728be7ecafdd5bfe65601
wassim-naai-vinci <wassim.naai@student.vinci.be>
2024-11-14 15:56:29 +0100

    refactor : format code with Prettier for consistency

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,125 +1,110 @@
 import { BerlinClock } from "../src/app.js";
 
 describe("minute line", function () {
+  it("should return 'OOOO' when the time is 0:00", function () {
+    const time = "0:00";
 
-    
-    it("should return 'OOOO' when the time is 0:00", function () {
-        const time = "0:00";
+    const result = new BerlinClock().oneMinuteLine(time);
 
-        const result = new BerlinClock().oneMinuteLine(time);
+    expect(result).toBe("OOOO");
+  });
 
-        expect(result).toBe("OOOO");
-    });
+  it("should return 'YOOO' when the time is 0:01 ", function () {
+    const time = "0:01";
 
-    it("should return 'YOOO' when the time is 0:01 ", function () {
-        
-        const time = "0:01";
+    const result = new BerlinClock().oneMinuteLine(time);
 
-        const result = new BerlinClock().oneMinuteLine(time);
+    expect(result).toBe("YOOO");
+  });
 
-        expect(result).toBe("YOOO");
-    });
+  it("should return 'YYOO' when the time is 0:02", function () {
+    const time = "0:02";
 
-    it("should return 'YYOO' when the time is 0:02", function () {
-       
-        const time = "0:02";
+    const result = new BerlinClock().oneMinuteLine(time);
 
-        const result = new BerlinClock().oneMinuteLine(time);
+    expect(result).toBe("YYOO");
+  });
 
-        expect(result).toBe("YYOO");
-    });
+  it("should return 'YYYO' when the time is 0:03", function () {
+    const time = "0:03";
 
-    it("should return 'YYYO' when the time is 0:03", function () {
-       
-        const time = "0:03";
+    const result = new BerlinClock().oneMinuteLine(time);
 
-        const result = new BerlinClock().oneMinuteLine(time);
+    expect(result).toBe("YYYO");
+  });
 
-        expect(result).toBe("YYYO");
-    });
-    
-    it("should return 'YYYY' when the time is 0:04", function () {
-       
-        const time = "0:04";
+  it("should return 'YYYY' when the time is 0:04", function () {
+    const time = "0:04";
 
-        const result = new BerlinClock().oneMinuteLine(time);
+    const result = new BerlinClock().oneMinuteLine(time);
 
-        expect(result).toBe("YYYY");
-    });
+    expect(result).toBe("YYYY");
+  });
 
-    
-    it("should return 'OOOO' when given 0:05 ", function () {
-        const time = "0:05";
+  it("should return 'OOOO' when given 0:05 ", function () {
+    const time = "0:05";
 
-        const result = new BerlinClock().oneMinuteLine(time);
+    const result = new BerlinClock().oneMinuteLine(time);
 
-        expect(result).toBe("OOOO");
-    });
+    expect(result).toBe("OOOO");
+  });
 });
 
 // TEST FIVE MINUTES BLOCK à partir de 6
-describe("Five minutes block", function () {    
-    it("should return 'OOOOOOOOOOO' when the time is 0:00", function () {
+describe("Five minutes block", function () {
+  it("should return 'OOOOOOOOOOO' when the time is 0:00", function () {
+    const time = "0:00";
+    const result = new BerlinClock().fiveMinutesBlock(time);
 
-      const time = "0:00";
-      const result = new BerlinClock().fiveMinutesBlock(time);
-  
-      expect(result).toBe("OOOOOOOOOOO");
-    });
+    expect(result).toBe("OOOOOOOOOOO");
+  });
 
-    it("should return 'YOOOOOOOOOO' when the time is 0:05", function () {
+  it("should return 'YOOOOOOOOOO' when the time is 0:05", function () {
+    const time = "0:05";
+    const result = new BerlinClock().fiveMinutesBlock(time);
 
-      const time = "0:05";
-      const result = new BerlinClock().fiveMinutesBlock(time);
-  
-      expect(result).toBe("YOOOOOOOOOO");
-    });
+    expect(result).toBe("YOOOOOOOOOO");
+  });
 
-    it("should return 'YYOOOOOOOOO' when the time is 0:10", function () {
+  it("should return 'YYOOOOOOOOO' when the time is 0:10", function () {
+    const time = "0:10";
+    const result = new BerlinClock().fiveMinutesBlock(time);
 
-      const time = "0:10";
-      const result = new BerlinClock().fiveMinutesBlock(time);
-  
-      expect(result).toBe("YYOOOOOOOOO");
-    });
+    expect(result).toBe("YYOOOOOOOOO");
+  });
 
-    it("should return 'YYROOOOOOOO' when the time is 0:15", function () {
+  it("should return 'YYROOOOOOOO' when the time is 0:15", function () {
+    const time = "0:15";
+    const result = new BerlinClock().fiveMinutesBlock(time);
 
-      const time = "0:15";
-      const result = new BerlinClock().fiveMinutesBlock(time);
-  
-      expect(result).toBe("YYROOOOOOOO");
-    });
+    expect(result).toBe("YYROOOOOOOO");
+  });
 
-    it("should return 'YYRYYROOOOO' when the time is 0:30", function () {
+  it("should return 'YYRYYROOOOO' when the time is 0:30", function () {
+    const time = "0:30";
+    const result = new BerlinClock().fiveMinutesBlock(time);
 
-      const time = "0:30";
-      const result = new BerlinClock().fiveMinutesBlock(time);
-  
-      expect(result).toBe("YYRYYROOOOO");
-    });
+    expect(result).toBe("YYRYYROOOOO");
+  });
 
-    it("should return 'YYRYYRYYRYY' when the time is 0:55", function () {
+  it("should return 'YYRYYRYYRYY' when the time is 0:55", function () {
+    const time = "0:55";
+    const result = new BerlinClock().fiveMinutesBlock(time);
 
-      const time = "0:55";
-      const result = new BerlinClock().fiveMinutesBlock(time);
-  
-      expect(result).toBe("YYRYYRYYRYY");
-    });
-    // FINI LES TEST A 12
+    expect(result).toBe("YYRYYRYYRYY");
+  });
+  // FINI LES TEST A 12
 });
 
-describe("One hour line", function () {    
+describe("One hour line", function () {
   it("should display 'OOOO' when the time is 0:00", function () {
+    const time = "0:00";
+    const result = new BerlinClock().oneHourLine(time);
 
-      const time = "0:00";
-      const result = new BerlinClock().oneHourLine(time);
-
-      expect(result).toBe("OOOO");
+    expect(result).toBe("OOOO");
   });
 
   it("should display 'ROOO' when the time is 1:00", function () {
-
     const time = "1:00";
     const result = new BerlinClock().oneHourLine(time);
 
@@ -127,7 +112,6 @@ describe("One hour line", function () {
   });
 
   it("should display 'RROO' when the time is 2:00", function () {
-
     const time = "2:00";
     const result = new BerlinClock().oneHourLine(time);
 
@@ -135,7 +119,6 @@ describe("One hour line", function () {
   });
 
   it("should display 'RRRO' when the time is 3:00", function () {
-
     const time = "3:00";
     const result = new BerlinClock().oneHourLine(time);
 
@@ -143,7 +126,6 @@ describe("One hour line", function () {
   });
 
   it("should display 'RRRR' when the time is 4:00", function () {
-
     const time = "4:00";
     const result = new BerlinClock().oneHourLine(time);
 
@@ -155,12 +137,11 @@ describe("Five hours block", function () {
   const clock = new BerlinClock();
 
   it("should return 'OOOO' when given 0:00", function () {
-      const time = "0:00";
+    const time = "0:00";
 
-      const result = clock.fiveHourLine(time);
+    const result = clock.fiveHourLine(time);
 
-      expect(result).toBe("OOOO");
-
+    expect(result).toBe("OOOO");
   });
 
   it(" should return 'ROOO' when given 5:00", function () {
@@ -169,81 +150,75 @@ describe("Five hours block", function () {
     const result = clock.fiveHourLine(time);
 
     expect(result).toBe("ROOO");
-});
+  });
 
-it(" should return 'RROO' when given 10:00", function () {
-  const time = "10:00";
+  it(" should return 'RROO' when given 10:00", function () {
+    const time = "10:00";
 
-  const result = clock.fiveHourLine(time);
+    const result = clock.fiveHourLine(time);
 
-  expect(result).toBe("RROO");
-});
+    expect(result).toBe("RROO");
+  });
 
-it("should return 'RRRO' lamps when given 15:00", function () {
-  const time = "15:00";
+  it("should return 'RRRO' lamps when given 15:00", function () {
+    const time = "15:00";
 
-  const result = clock.fiveHourLine(time);
+    const result = clock.fiveHourLine(time);
 
-  expect(result).toBe("RRRO");
-});
-it(" should return 'RRRR' when given 20:00", function () {
-  const time = "20:00";
+    expect(result).toBe("RRRO");
+  });
+  it(" should return 'RRRR' when given 20:00", function () {
+    const time = "20:00";
 
-  const result = clock.fiveHourLine(time);
+    const result = clock.fiveHourLine(time);
 
-  expect(result).toBe("RRRR");
-});
+    expect(result).toBe("RRRR");
+  });
 
-it("should return 'OOOO' when given 1:00", function () {
-  const time = "1:00";
+  it("should return 'OOOO' when given 1:00", function () {
+    const time = "1:00";
 
-  const result = clock.fiveHourLine(time);
+    const result = clock.fiveHourLine(time);
 
-  expect(result).toBe("OOOO");
-});
+    expect(result).toBe("OOOO");
+  });
 
-it("should return 'RROO' when given 14:00", function () {
-  const time = "14:00";
+  it("should return 'RROO' when given 14:00", function () {
+    const time = "14:00";
 
-  const result = clock.fiveHourLine(time);
+    const result = clock.fiveHourLine(time);
 
-  expect(result).toBe("RROO");
-});
+    expect(result).toBe("RROO");
+  });
 });
 
 describe("Seconds lamp", function () {
   it("should return 'R' when the seconds are even", function () {
-      const time = "0:00:30";
-      const result = new BerlinClock().secondsLamp(time);
-      expect(result).toBe("R");
+    const time = "0:00:30";
+    const result = new BerlinClock().secondsLamp(time);
+    expect(result).toBe("R");
   });
   it("should return 'O' when the seconds are odd", function () {
     const time = "0:00:31";
     const result = new BerlinClock().secondsLamp(time);
     expect(result).toBe("O");
-});
+  });
 });
 
 describe("Berlin Clock", function () {
+  it("BerlinClock should return R:ROOO:OOOO:YOOOOOOOOOO:YOOO when given 05:06:00", function () {
+    const time = "05:06:00";
+    const result = new BerlinClock().entireClock(time);
 
-    it("BerlinClock should return R:ROOO:OOOO:YOOOOOOOOOO:YOOO when given 05:06:00", function() {
+    expect(result).toBe("R\nROOO\nOOOO\nYOOOOOOOOOO\nYOOO");
+  });
 
-        const time = "05:06:00";
-        const result = new BerlinClock().entireClock(time);
+  describe("Berlin Clock", function () {
+    it("BerlinClock should return O:RRRR:ROOO:YYRYYRYYROO:OOOO when given 21:45:37", function () {
+      const time = "21:45:37";
+      const result = new BerlinClock().entireClock(time);
 
-        expect(result).toBe("R\nROOO\nOOOO\nYOOOOOOOOOO\nYOOO");
+      expect(result).toBe("O\nRRRR\nROOO\nYYRYYRYYROO\nOOOO");
     });
-
-    describe("Berlin Clock", function () {
-
-        it("BerlinClock should return O:RRRR:ROOO:YYRYYRYYROO:OOOO when given 21:45:37", function() {
-    
-            const time = "21:45:37";
-            const result = new BerlinClock().entireClock(time);
-    
-            expect(result).toBe("O\nRRRR\nROOO\nYYRYYRYYROO\nOOOO");
-        });
-    
-    });
-
+  });
 });
--- src/app.js
+++ src/app.js
@@ -1,67 +1,66 @@
 export class BerlinClock {
+  oneMinuteLine(time) {
+    const minutes = parseInt(time.split(":")[1], 10);
+    const lampsToLight = minutes % 5;
 
-    oneMinuteLine(time) {
-
-        const minutes = parseInt(time.split(":")[1], 10); 
-        const lampsToLight = minutes % 5;
-
-        if (lampsToLight === 0) {
-            return "OOOO";
-        } else {
-            const lampesText = lampsToLight === 1 ? "YOOO" : "Y".repeat(lampsToLight) + "O".repeat(4 - lampsToLight);
-            return lampesText;
-        }
+    if (lampsToLight === 0) {
+      return "OOOO";
+    } else {
+      const lampesText =
+        lampsToLight === 1
+          ? "YOOO"
+          : "Y".repeat(lampsToLight) + "O".repeat(4 - lampsToLight);
+      return lampesText;
     }
-    
-    fiveMinutesBlock(time) {
-        // re commit pour etre sur d'avoir fait le refactor 
-        const minutes = parseInt(time.split(":")[1], 10);// exemple 0:05 => 5 minutes , 0:15 => 15 minutes
+  }
 
-        let line = '';
-        const LampsOn = Math.floor(minutes / 5);
-    
-        for (let i = 1; i <= 11; i++) {
-            if (i <= LampsOn) {
-                line += (i % 3 === 0) ? 'R' : 'Y';
-            } else {
-                line += 'O';
-            }
-        }
+  fiveMinutesBlock(time) {
+    // re commit pour etre sur d'avoir fait le refactor
+    const minutes = parseInt(time.split(":")[1], 10); // exemple 0:05 => 5 minutes , 0:15 => 15 minutes
 
-        return line;
+    let line = "";
+    const LampsOn = Math.floor(minutes / 5);
+
+    for (let i = 1; i <= 11; i++) {
+      if (i <= LampsOn) {
+        line += i % 3 === 0 ? "R" : "Y";
+      } else {
+        line += "O";
+      }
     }
 
-    oneHourLine(time) {
-        const hours = parseInt(time.split(":")[0], 10) % 5; // Exemple : 12:00 => 12 % 5 = 2 (2 lampes rouges allumées)
-    
-        let line = '';
-        for (let i = 1; i <= 4; i++) {
-            line += (i <= hours) ? 'R' : 'O';
-        }
-    
-        return line;
+    return line;
+  }
+
+  oneHourLine(time) {
+    const hours = parseInt(time.split(":")[0], 10) % 5; // Exemple : 12:00 => 12 % 5 = 2 (2 lampes rouges allumées)
+
+    let line = "";
+    for (let i = 1; i <= 4; i++) {
+      line += i <= hours ? "R" : "O";
     }
 
-    fiveHourLine(time) {
-       
+    return line;
+  }
+
+  fiveHourLine(time) {
     const hours = parseInt(time.split(":")[0], 10); // Extraire les heures de l'entrée "hh:mm"
     const numberOfLamps = Math.floor(hours / 5); // Calculer combien de blocs de 5 heures
 
     return "R".repeat(numberOfLamps) + "O".repeat(4 - numberOfLamps); // Remplir avec "R" et "O"
-     
-    }
+  }
 
-    secondsLamp(time) {
-        const seconds = parseInt(time.split(":")[2], 10);
-        return seconds % 2 === 0 ? "R" : "O"; // "R" pour rouge (allumé), "O" pour éteint
-    }
-    
-    entireClock(time){ 
-        const second = this.secondsLamp(time);
-        const fiveHours = this.fiveHourLine(time);
-        const simpleHour = this.oneHourLine(time);
-        const fiveMinutes = this.fiveMinutesBlock(time);
-        const simpleMinutes = this.oneMinuteLine(time);
-        return `${second}\n${fiveHours}\n${simpleHour}\n${fiveMinutes}\n${simpleMinutes}`;
-    }
+  secondsLamp(time) {
+    const seconds = parseInt(time.split(":")[2], 10);
+    return seconds % 2 === 0 ? "R" : "O"; // "R" pour rouge (allumé), "O" pour éteint
+  }
+
+  entireClock(time) {
+    const second = this.secondsLamp(time);
+    const fiveHours = this.fiveHourLine(time);
+    const simpleHour = this.oneHourLine(time);
+    const fiveMinutes = this.fiveMinutesBlock(time);
+    const simpleMinutes = this.oneMinuteLine(time);
+    return `${second}\n${fiveHours}\n${simpleHour}\n${fiveMinutes}\n${simpleMinutes}`;
+  }
 }
